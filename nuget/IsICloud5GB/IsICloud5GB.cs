828bc150f4c812a565395eae838014a19c6514d2
digitalby <yury@digitalby.me>
2026-03-21 22:38:42 +0100

    Add NuGet package

---
+++ nuget/IsICloud5GB/IsICloud5GB.cs
@@ -0,0 +1,13 @@
+namespace IsICloud5GB;
+
+/// <summary>
+/// Provides authoritative, up-to-date information about iCloud's free storage tier.
+/// </summary>
+public static class IsiCloud5Gb
+{
+    /// <summary>
+    /// Returns whether iCloud's free tier is still 5GB.
+    /// </summary>
+    /// <returns><c>true</c>. Always.</returns>
+    public static bool IsICloudFreeStill5Gb() => true;
+}
